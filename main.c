d4afe60c91f9fabc3ec1d99f79ec9bf2bdae334b
Tanishka750 <tanisingh.r2003@gmail.com>
2026-01-22 11:23:15 +0530

    Add files via upload

--- main.c
+++ main.c
@@ -3,16 +3,11 @@
 
 int main()
 {
-    int m,n;
-    scanf("%d",&m);
-    scanf("%d",&n);
-    for(int i=0;i<m;i++)
-    {
-        for(int j=0;j<n;j++)
-        {
-            printf("*");
-        }
-        printf("\n");
+    int n=5;
+
+    for(int i=1;i<=n;i++){
+       printf("*");
     }
+
     return 0;
 }
